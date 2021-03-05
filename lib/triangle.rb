7e8fc1bb0f0323fc210fb22cfea26b0dc470e147
johnnygibbs95 <johnnydgibbs1995@gmail.com>
2021-03-04 21:27:42 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    if side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2
+      raise TriangleError
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    end
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 == @side2 && @side1 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      :isosceles
+    else
+      :scalene
+    end 
+  end
+  class TriangleError < StandardError
+    def message
+      puts "This is not a triangle"
+    end
+  end
 end
+
+triangle = Triangle.new(1, 1, 1)
+triangle.kind
